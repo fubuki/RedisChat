54ab1afb01c0069a196d22ca669f8dfbaeb300d5
fubuki <fubuki@users.noreply.github.com>
2014-04-27 22:22:12 +0800

    Update server.js
    更新部份socket.io設定

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-var io = require('socket.io').listen(3000);
+var io = require('socket.io')(3000, { /* options */ });
 var redis = require("redis");
 var port="6379";
 var host="127.0.0.1"
 var redisClient = redis.createClient(port, host);
-var pub = redis.createClient(port, host); 
-var sub = redis.createClient(port, host); 
+var pub = redis.createClient(port, host);
+var sub = redis.createClient(port, host);
 var channel = 'chatting';
 
 sub.psubscribe("chatting");
@@ -13,6 +13,12 @@ sub.on("pmessage", function(pat, ch, msg) {
   console.log(ch);
   io.sockets. in (ch).emit('news', msg);
 });
+
+io.use(function(socket, next) {
+  var handshakeData = socket.request;
+  next();
+});
+
 
 io.sockets.on('connection', function (client) {
     client.join(channel);

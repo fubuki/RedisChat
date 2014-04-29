885b1eb1913cf79989b79615b099ee37234c0550
fubuki <fubuki@users.noreply.github.com>
2014-04-29 21:40:44 +0800

    socket.io 加入 cluster的模組。

--- server/server.js
+++ server/server.js
@@ -8,24 +8,52 @@ var pub = redis.createClient(port, host);
 var sub = redis.createClient(port, host);
 var channel = 'chatting';
 
-var io = socketio(3000, {});
-
-io.adapter(RedisStore({ host: host, port: port }));
-
-sub.psubscribe("chatting");
-
-sub.on("pmessage", function(pat, ch, msg) {
-  console.log(ch);
-  io.sockets. in (ch).emit('news', msg);
-});
-
-io.use(function(socket, next) {
-  var handshakeData = socket.request;
-  next();
-});
+var cluster = require('cluster'), 
+numCPUs = require('os').cpus().length;
 
 
-io.sockets.on('connection', function (client) {
+
+
+if (cluster.isMaster) {
+  console.log('master');
+  var redisClient = redis.createClient(port, host);
+  var pub = redis.createClient(port, host);
+  var sub = redis.createClient(port, host);
+  var io = socketio({});
+
+  io.adapter(RedisStore({ host: host, port: port }));
+
+  sub.psubscribe("chatting");
+
+  sub.on("pmessage", function(pat, ch, msg) {
+    console.log(ch);
+    io.sockets. in (ch).emit('news', msg);
+  });
+
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+
+} else {
+  console.log('child');
+  var redisClient = redis.createClient(port, host);
+  var pub = redis.createClient(port, host);
+  var sub = redis.createClient(port, host);
+  var io = socketio(3000, {});
+
+  io.adapter(RedisStore({ host: host, port: port }));
+
+
+  io.use(function(socket, next) {
+    var handshakeData = socket.request;
+    next();
+  });
+
+  io.sockets.on('connection', function (client) {
     client.join(channel);
     client.on("typing", function(content) {
       console.log(content);
@@ -40,4 +68,14 @@ io.sockets.on('connection', function (client) {
       console.log('disconnect');
       client.leave(channel);
     });
-});
+  });
+
+
+}
+
+
+
+
+
+
+

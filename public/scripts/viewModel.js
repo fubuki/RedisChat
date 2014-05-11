9e821febed649f32055ef342c356ac9f76b26b17
fubuki <fubuki@users.noreply.github.com>
2014-05-11 21:42:46 +0800

    使用ko綁定keydown事件。

--- public/scripts/viewModel.js
+++ public/scripts/viewModel.js
@@ -9,36 +9,41 @@
 
 	var $ = window.jQuery,
 		ko = window.ko,
+		socket,
 		viewModel;
 
+	var socketHost = 'http://192.168.65.141:80';
 
+	var socketPath = '/web/socket.io';
 
 	var init = function () {
 		ko.applyBindings(viewModel, $("#main").get(0));
 
-		var socket = io.connect('http://192.168.65.141:80',{path: '/web/socket.io' });
+		socket = io.connect(socketHost,{path: socketPath });
 		socket.on('news', function(data) {
 			var json = JSON.parse(data);
 
 			viewModel.historyList.push(json);
-
- 			//$("#content").append('<li>'+json.content+'</li>');
 		});
 		
-		$('#text_input').keydown(function(h) {
-			if(h.keyCode == 13) {
-				var message = $(this).val();
-				socket.emit('typing', message);
-				$(this).val('');
-			}
-		});
 	};
 
 
 	viewModel = (function () {
 		return {
 
-			historyList: ko.observableArray([])
+			historyList: ko.observableArray([]),
+
+			inputMessage: function (element, event) {
+
+				if(event.keyCode == 13) {
+					var message = $(event.currentTarget).val();
+					socket.emit('typing', message);
+					$(event.currentTarget).val('');
+				}
+
+				return true;
+			}
 
 		};
 	})();
